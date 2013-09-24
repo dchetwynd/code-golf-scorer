538d8a9b845ea58316e96f842b60e083ce552abb
Daley Chetwynd <d_chetwynd@fastmail.co.uk>
2013-09-24 19:32:08 +0100

    Made script executable

--- code_golf_scorer.py
+++ code_golf_scorer.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 
 class CodeGolfScorer:
     
@@ -8,3 +9,12 @@ class CodeGolfScorer:
 	    contents = f.read()
 	    contents_without_whitespace = re.sub(r'\s', '', contents)
 	    return len(contents_without_whitespace)
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit("You must provide a filename argument")
+
+    scorer = CodeGolfScorer()
+    character_count = scorer.score(sys.argv[1])
+
+    print "The file has %d characters" % character_count
